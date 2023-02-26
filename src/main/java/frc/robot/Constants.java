e3e6ea309caa1ab7cd77fae3aaf3161ef519b8c0
zbrantmeier <brantmeierz@gmail.com>
2023-02-26 11:56:23 -0600

    enhancement

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -156,7 +156,7 @@ public final class Constants {
         public static final int intakeMotorCANID = 1;
 
         /* Elevator Position constants */
-        //Gatorvator
+        //Gatorvator 🐊
 
         public static final double ELEVATOR_HOME_POSITION = 0;
         public static final double ELEVATOR_LOW_POSITION = 0;
