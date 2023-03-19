9ac53423c5508cf430e6e665ab4f193eb780c138
Dilpreet Randhawa <dsrandhawa3244@gmail.com>
2023-03-19 13:18:14 -0500

    push with various teleop fixes + updated sys id constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -75,9 +75,9 @@ public final class Constants {
 
         /* Drive Motor Characterization Values
          * Divide SYSID values by 12 to convert from volts to percent output for CTRE */
-        public static final double driveKS = (0.12776 / 12);
-        public static final double driveKV = (2.3231 / 12);
-        public static final double driveKA = (0.31662 / 12);
+        public static final double driveKS = (0.15848 / 12);
+        public static final double driveKV = (2.2698 / 12);
+        public static final double driveKA = (0.32845 / 12);
 
         /* Swerve Profiling Values */
         /** Meters per Second */
