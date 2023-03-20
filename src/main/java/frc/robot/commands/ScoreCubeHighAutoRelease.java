3b95d97bc6cc99b37a2910a72981b65f71558c48
zbrantmeier <brantmeierz@gmail.com>
2023-03-19 19:19:43 -0500

    last push Zach 3-19-23

--- src/main/java/frc/robot/commands/ScoreCubeHighAutoRelease.java
+++ src/main/java/frc/robot/commands/ScoreCubeHighAutoRelease.java
@@ -16,6 +16,7 @@ public class ScoreCubeHighAutoRelease extends SequentialCommandGroup {
   public ScoreCubeHighAutoRelease() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new SetWristIntakeSpeed(-1).withTimeout(1));
+    addCommands(new SetWristIntakeSpeed(-1).withTimeout(1),
+    new ReturnWAEHomeTimeout());
   }
 }
